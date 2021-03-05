820c70ec8c049df7fabebd9b696c442f2f53b8d1
Sree Nithish B <sreenithishbofficial@gmail.com>
2021-03-06 00:30:54 +0530

    Added signals & parsed json

--- dashboard/consumers.py
+++ dashboard/consumers.py
@@ -1,13 +1,26 @@
 import json
-from random import randint
-from time import sleep
+from .models import users
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
 from channels.generic.websocket import WebsocketConsumer
+
+temp_self = None
 
 class WSConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
 
-        for i in range(3):
-            self.send(json.dumps({'message': randint(1, 1000)}))
-            sleep(1)
+        global temp_self
+
+        temp_self = self
+
+        temp_self.send(json.dumps({'message': users.get_all_objects(users)}))
+
+
+@receiver(post_save, sender=users)
+@receiver(post_delete, sender=users)
+def get_model_objects(sender, **kwargs):
+    print(users.get_all_objects(users))
+
+    temp_self.send(json.dumps({'message': users.get_all_objects(users)}))
